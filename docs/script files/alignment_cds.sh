de5c430cdb9a8f8d80019bf2405785256d5a7721
SOTIRIA BOUTSI <sotiriabgm@gmail.com>
2022-11-01 12:22:43 +0000

    update

---
+++ docs/script files/alignment_cds.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+for i in ./raw_data_per_sample/*.fastq
+do
+echo ${i}
+./software/bowtie2-2.4.5-linux-x86_64/bowtie2 -x ./reference/index_cds_pum -U ${i} -S ${i}_cds.sam
+done
+
+mv ./raw_data_per_sample/*.sam ./alignment_results
+
